0b8c175d6c17f8787ae3504c39f59db2e5dc3beb
Bogdan Bryzh <bogdan.brizhwasd@gmail.com>
2018-12-31 19:41:25 +0200

    efm: commit

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
+const fs = require('fs-extra')
 
 console.log(process.platform);
 
--- index.js
+++ index.js
@@ -1,5 +1,50 @@
-const { app, BrowserWindow } = require('electron')
+const electron = require( 'electron' ),
+	  url = require( 'url' ),
+	  path = require( 'path' )
+
+const = { app, BrowserWindow, globalShortcut } = require('electron')
+
+let MainWindow
 
 const boot = () => {
-	
+	MainWindow = new BrowserWindow({
+		width: 600,
+		height: 700,
+		show: false,
+		frame: false,
+		webPreferences: {
+			nodeIntegration: true
+		},
+		icon: './icon.png'
+	})
+
+	MainWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'app/index.html')
+	}))
+
+	MainWindow.once('ready-to-show', () => {
+		MainWindow.show()
+	})
+
+	MainWindow.on('closed', () => {
+		MainWindow = null
+	})
+
+	globalShortcut.register('F5', () => {
+		MainWindow.reload()
+	})
 }
+
+app.on('ready', boot)
+
+app.on('window-all-closed', () => {
+	if ( process.platform !== 'darwin' ) {
+		app.quit()
+	}
+})
+
+app.on('activate', () => {
+	if ( MainWindow === null ) {
+		boot()
+	}
+})
